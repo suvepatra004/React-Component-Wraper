282158db1feafbee18e7538dd7675324fdbdd69c
Suvendu Kumar Patra <suvepatra2003@gmail.com>
2026-04-01 17:33:33 +0530

    feat: Add routes of patient, doctor, appointment

--- Hospital-Management/backend/server.js
+++ Hospital-Management/backend/server.js
@@ -20,8 +20,99 @@ const Patient = require("./models/Patient");
 const Doctor = require("./models/Doctor");
 const Appointment = require("./models/Appointment");
 
-// Patient Routes
-// Doctor Routes
-// Appointment Routes
+// ---------- Patient Routes ----------
+app.get("/api/patients", async (req, res) => {
+  const patients = await Patient.find().sort({ createdAt: -1 });
+  res.json(patients);
+});
+
+app.post("/api/patients/", async (req, res) => {
+  const patients = await Patient.create(req.body);
+  res.status(201).json(patients);
+});
+
+app.put("/api/patients/:id", async (req, res) => {
+  const updatedPatient = await Patient.findByIdAndUpdate(
+    req.params.id,
+    req.body,
+    {
+      new: true,
+    },
+  );
+  res.json(updatedPatient);
+});
+
+app.delete("/api/patients/:id", async (req, res) => {
+  await Patient.findByIdAndDelete(req.params.id);
+  res.json({ message: `Patient deleted successfully.` });
+});
+
+// -------- Doctor Routes ------------
+app.get("/api/doctors", async (req, res) => {
+  const doctor = await Doctor.find().sort({ name: 1 });
+  res.json(doctor);
+});
+
+app.post("/api/doctors", async (req, res) => {
+  const doctor = await Doctor.create(req.body);
+  res.status(201).json(doctor);
+});
+
+app.put("/api/doctors/:id", async (req, res) => {
+  const updatedDoctor = await Doctor.findByIdAndUpdate(
+    req.params.id,
+    req.body,
+    {
+      new: true,
+    },
+  );
+  res.json(updatedDoctor);
+});
+
+app.delete("/api/doctors/:id", async (req, res) => {
+  await Doctor.findByIdAndDelete(req.params.id);
+  res.json({ message: `${req.body} deleted successfully` });
+});
+
+// ----------- Appointment Routes ------------
+app.get("/api/appointments", async (req, res) => {
+  const appointments = await Appointment.find()
+    .populate("patient", "name")
+    .populate("doctor", "name specialization")
+    .sort({ createdAt: -1 });
+
+  res.json(appointments);
+});
+
+app.post("/api/appointments", async (req, res) => {
+  const appointment = await Appointment.create(req.body);
+  res.status(201).json(appointment);
+});
+
+app.put("/api/appointments/:id", async (req, res) => {
+  const updatedAppointment = await Appointment.findByIdAndUpdate(
+    req.params.id,
+    req.body,
+    { new: true },
+  );
+  res.json(updatedAppointment);
+});
+
+app.delete("/api/appointments/:id", async (req, res) => {
+  await Appointment.findByIdAndDelete(req.params.id);
+  res.json({ message: `${req.body} deleted successfully` });
+});
+
 // Dashboard Stats
+app.get("/api/stats", async (req, res) => {
+  const [patients, doctors, appointments] = await Promise.all([
+    Patient.countDocuments(),
+    Doctor.countDocuments(),
+    Appointment.countDocuments(),
+  ]);
+  res.json({ patients, doctors, appointments });
+});
 // Connection to PORT=5000
+app.listen(5000 || process.env.PORT, () => {
+  console.log(`Server is running at PORT ${process.env.PORT}`);
+});
