508c301452bf0d7641f5063098f05a831091cca8
Suvendu Kumar Patra <suvepatra2003@gmail.com>
2026-04-02 23:58:17 +0530

    feat: set up initial structure with App component, CSS styles, and new pages for Appointments, Dashboard, Doctors, and Patients

--- Hospital-Management/Frontend/src/main.jsx
+++ Hospital-Management/Frontend/src/main.jsx
@@ -1,10 +1,23 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);
+
+/**
+ *
+ * TODO:
+ *
+ * [] Add api.js file - That will handle all http request methods
+ * [] Add index.css file
+ * [] Add Dashboard.jsx file
+ * [] Add Patients.jsx file
+ * [] Add Doctors.jsx file
+ * [] Add Appointments.jsx file
+ */
