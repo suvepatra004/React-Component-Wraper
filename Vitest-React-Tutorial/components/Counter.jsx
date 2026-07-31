e9af8607b4cf2e284ffb3d570a74b06a01a05a9e
Suvendu Kumar Patra <suvepatra2003@gmail.com>
2026-07-31 22:29:58 +0530

    feat: initialize Vitest React tutorial with basic setup

---
+++ Vitest-React-Tutorial/components/Counter.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Counter = () => {
+  return (
+    <div>
+        <h1>Counter Component</h1>
+        <p>This is just a normal Counter Button</p>
+    </div>
+  )
+}
+
+export default Counter
