079a87aed6ca32c1443eee8e273f365acaeb0c5f
Suvendu Kumar Patra <suvepatra2003@gmail.com>
2026-08-16 17:18:17 +0530

    fix: update base path in Vite configuration for GitProfile-Dashboard

--- GitProfile-Dashboard/vite.config.js
+++ GitProfile-Dashboard/vite.config.js
@@ -4,7 +4,7 @@ import babel from "@rolldown/plugin-babel";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  base: "/React-Components/",
+  base: "/",
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] }),
